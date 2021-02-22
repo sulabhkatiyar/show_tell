32f09a53e97dd911fe9f7525c668e37315b591a0
Sulabh <60035864+sulabhkatiyar@users.noreply.github.com>
2021-02-22 22:48:26 +0530

    Update create_input_files_coco.py

--- create_input_files_coco.py
+++ create_input_files_coco.py
@@ -1,5 +1,10 @@
 from utils import create_input_files
 
+"""
+To create files that contain all images stored in h5py format and captions stored in json files.
+Minimum word frequencies to be used as cut-off for removing rare words to be specifiied here.
+"""
+
 if __name__ == '__main__':
     create_input_files(dataset='coco',
                        karpathy_json_path='path_to___dataset_coco.json',
