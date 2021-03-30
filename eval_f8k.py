acf8e177602f14b3725a95e172684b3dbb4749a5
Sulabh <60035864+sulabhkatiyar@users.noreply.github.com>
2021-03-30 15:21:59 +0530

    Update eval_f8k.py

--- eval_f8k.py
+++ eval_f8k.py
@@ -13,10 +13,10 @@ import argparse
 
 
 # Parameters
-data_folder = '/home/sulabh02/for_jupyter/papers/iccv/pos/show_tell_vinyals/caption_data/'  # folder with data files saved by create_input_files.py
+data_folder = 'path_to_dat_folder'  # folder with data files saved by create_input_files.py, i.e. 'output_folder'
 data_name = 'flickr8k_5_cap_per_img_5_min_word_freq'   # base name shared by data files
 checkpoint = 'BEST_checkpoint_flickr8k_5_cap_per_img_5_min_word_freq.pth.tar'  # model checkpoint
-word_map_file = '/home/sulabh02/for_jupyter/papers/iccv/pos/show_tell_vinyals/caption_data/WORDMAP_flickr8k_5_cap_per_img_5_min_word_freq.json'  # word map, ensure it's the same the data was encoded with and the model was trained with
+word_map_file = data_folder + '/WORDMAP_flickr8k_5_cap_per_img_5_min_word_freq.json'  # word map, ensure it's the same the data was encoded with and the model was trained with
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # sets device for model and PyTorch tensors
 cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhead
 
@@ -117,71 +117,15 @@ def evaluate(beam_size):
 
         # Start decoding
         step = 1
-        if choice==0:
-            # h, c = decoder.init_hidden_state(encoder_out)
-            # h1, c1 = h.clone(), c.clone()
-            # h2, c2 = h.clone(), c.clone()
-            # h3, c3 = h.clone(), c.clone()
-            # h, c, h1, c1, _, _, _, _ = decoder.init_hidden_state(encoder_out)     # Initialize LSTM state
-            # h, c, h1, c1, h2, c2, _, _ = decoder.init_hidden_state(encoder_out)     # Initialize LSTM state
-            # h, c, h1, c1, h2, c2, h3, c3 = decoder.init_hidden_state(encoder_out)     # Initialize LSTM state
-            # h, c, h1, c1, h2, c2, h3, c3, h4, c4 = decoder.init_hidden_state(encoder_out)     # Initialize LSTM state
-            h, c, h1, c1, h2, c2, h3, c3, h4, c4, h5, c5 = decoder.init_hidden_state(encoder_out)     # Initialize LSTM state
-
-
-        elif choice==1:
-            _,_,h_f,h_b,c_f,c_b = decoder.init_hidden_state_bidirectional(encoder_out)
-            # h = h_f.add(torch.flip(h_b, (0,)))
-            # c = c_f.add(torch.flip(c_b, (0,)))
-        elif choice==2:
-            h_1, h_2, h_3, c_1, c_2, c_3 = decoder.init_hidden_state_deeplstm(encoder_out)
+        h, c = decoder.init_hidden_state(encoder_out)
 
         # s is a number less than or equal to k, because sequences are removed from this process once they hit <end>
         while True:
-            # adding features tensor
-            # feature_map = decoder.features_tensor(encoder_out, k_prev_words).squeeze(1)
-
-            if embeddings_ensemble_available == True:
-                embeddings = decoder.final_embeddings(decoder.embedding(k_prev_words)).squeeze(1)  # (s, embed_dim)
-            else:
-                embeddings = decoder.embedding(k_prev_words).squeeze(1)
-            # assert(feature_map.shape == embeddings.shape)
-            # embeddings = embeddings + feature_map
-            #awe, _ = decoder.attention(encoder_out, h)  # (s, encoder_dim), (s, num_pixels)
-            #gate = decoder.sigmoid(decoder.f_beta(h))  # gating scalar, (s, encoder_dim)
-            #awe = gate * awe
-            if choice==0:
-                h, c = decoder.decode_step(embeddings, (h, c))  # (s, decoder_dim)
-                h1, c1 = decoder.decode_step1(h, (h1, c1))     # (batch_size_t, decoder_dim)
-                h2, c2 = decoder.decode_step2(h1, (h2, c2))     # (batch_size_t, decoder_dim)
-                h3, c3 = decoder.decode_step3(h2, (h3, c3))     # (batch_size_t, decoder_dim)
-                h4, c4 = decoder.decode_step4(h3, (h4, c4))     # (batch_size_t, decoder_dim)
-                h5, c5 = decoder.decode_step5(h4, (h5, c5))     # (batch_size_t, decoder_dim)
-
-            elif choice==1:
-                # print(embeddings.shape, h_f.shape)
-                h_f, c_f = decoder.decode_step_f(embeddings, (h_f, c_f))
-                # h_b, c_b = decoder.decode_step_b(torch.flip(embeddings, (0,1)), (h_b, c_b))
-                # h = h_f.add(torch.flip(h_b, (0,)))
-                # c = c_f.add(torch.flip(c_b, (0,)))
-                # h = h_f.add(h_b)
-                # c = c_f.add(c_b)
-            elif choice == 2:
-                h_1, c_1 = decoder.decode_step1(embeddings, (h_1, c_1))
-                h_2, c_2 = decoder.decode_step2(h_1, (h_2, c_2))
-                h_3, c_3 = decoder.decode_step3(h_2, (h_3, c_3))
-                h, c = h_3, c_3
-            # scores = decoder.fc(h)  # (s, vocab_size)
-            # scores = decoder.fc(h1)  # (s, vocab_size)
-            # scores = decoder.fc(h2)  # (s, vocab_size)
-            # scores = decoder.fc(h3)  # (s, vocab_size)
-            # scores = decoder.fc(h4)  # (s, vocab_size)
-            scores = decoder.fc(h5)  # (s, vocab_size)
-
+            embeddings = decoder.embedding(k_prev_words).squeeze(1)
+            h, c = decoder.decode_step(embeddings, (h, c))  # (s, decoder_dim)
+            scores = decoder.fc(h)  # (s, vocab_size)
 
             scores = F.log_softmax(scores, dim=1)
-
-            # Add
             scores = top_k_scores.expand_as(scores) + scores  # (s, vocab_size)
 
             # For the first step, all k points will have the same scores (since same k previous words, h, c)
@@ -216,25 +160,7 @@ def evaluate(beam_size):
             if choice==0:
                 h = h[prev_word_inds[incomplete_inds]]
                 c = c[prev_word_inds[incomplete_inds]]
-                h1 = h1[prev_word_inds[incomplete_inds]]
-                c1 = c1[prev_word_inds[incomplete_inds]]
-                h2 = h2[prev_word_inds[incomplete_inds]]
-                c2 = c2[prev_word_inds[incomplete_inds]]
-                h3 = h3[prev_word_inds[incomplete_inds]]
-                c3 = c3[prev_word_inds[incomplete_inds]]
-                h4 = h4[prev_word_inds[incomplete_inds]]
-                c4 = c4[prev_word_inds[incomplete_inds]]
-                h5 = h5[prev_word_inds[incomplete_inds]]
-                c5 = c5[prev_word_inds[incomplete_inds]]
 
-            elif choice==1:
-                h_f, c_f = h_f[prev_word_inds[incomplete_inds]], c_f[prev_word_inds[incomplete_inds]]
-                # h_b, c_b = torch.flip(h_b[prev_word_inds[incomplete_inds]], (0,)), torch.flip(c_b[prev_word_inds[incomplete_inds]], (0,))
-                # h_b, c_b = h_b[prev_word_inds[incomplete_inds]], c_b[prev_word_inds[incomplete_inds]]
-            elif choice==2:
-                h_1, c_1 = h_1[prev_word_inds[incomplete_inds]], c_1[prev_word_inds[incomplete_inds]]
-                h_2, c_2 = h_2[prev_word_inds[incomplete_inds]], c_2[prev_word_inds[incomplete_inds]]
-                h_3, c_3 = h_3[prev_word_inds[incomplete_inds]], c_3[prev_word_inds[incomplete_inds]]
             encoder_out = encoder_out[prev_word_inds[incomplete_inds]]
             top_k_scores = top_k_scores[incomplete_inds].unsqueeze(1)
             k_prev_words = next_word_inds[incomplete_inds].unsqueeze(1)
@@ -250,7 +176,7 @@ def evaluate(beam_size):
         except:
             seq = []
             empty_hypo += 1
-            # continue
+
 
         # References
         img_caps = allcaps[0].tolist()
