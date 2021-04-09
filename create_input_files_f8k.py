34113e4a861e2020208b8c567882a45fddf2f2cb
Sulabh <60035864+sulabhkatiyar@users.noreply.github.com>
2021-04-09 09:12:15 +0530

    Update create_input_files_f8k.py

--- create_input_files_f8k.py
+++ create_input_files_f8k.py
@@ -8,7 +8,7 @@ Minimum word frequencies to be used as cut-off for removing rare words to be spe
 if __name__ == '__main__':
     create_input_files(dataset='flickr8k',
                        karpathy_json_path='path_to___dataset_flickr8k.json',
-                       image_folder='path_to__mscoco_folder',
+                       image_folder='path_to_flickr8k_folder',
                        captions_per_image=5,
                        min_word_freq=5,
                        output_folder='folder_for_processed_data',
