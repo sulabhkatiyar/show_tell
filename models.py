8b10da26b55f09385495a89530801e7218f054de
Sulabh <60035864+sulabhkatiyar@users.noreply.github.com>
2021-04-22 22:46:24 +0530

    Update models.py

--- models.py
+++ models.py
@@ -47,10 +47,6 @@ class Encoder(nn.Module):
             y = self.classifier(x_feat)
             return y
             
-        out = self.adaptive_pool(out)  # (batch_size, 512, encoded_image_size, encoded_image_size)
-        out = out.permute(0, 2, 3, 1)  # (batch_size, encoded_image_size, encoded_image_size, 512)
-
-        return out
 
     def fine_tune(self, fine_tune=True):
         """
@@ -67,3 +63,123 @@ class Encoder(nn.Module):
                 for p in c.parameters():
                     p.requires_grad = fine_tune
 
+                    
+class DecoderWithAttention_choice(nn.Module):
+    """
+    Decoder.
+    """
+    """
+    We have a choice now as to what RNN we want to use. 0:LSTM, 1: Bidirectional LSTM, 2: GRU, 3: Bidirectional GRU
+    """
+    
+
+
+    def __init__(self, embed_dim, decoder_dim, vocab_size, encoder_dim=2048, dropout=0.5,choice=0, final_embeddings_dim=512, num_layers=2):
+        """
+        :param attention_dim: size of attention network
+        :param embed_dim: embedding size
+        :param decoder_dim: size of decoder's RNN
+        :param vocab_size: size of vocabulary
+        :param encoder_dim: feature size of encoded images
+        :param dropout: dropout
+        """
+        super(DecoderWithAttention_choice, self).__init__()
+
+        self.encoder_dim = encoder_dim
+        self.embed_dim = embed_dim
+        self.final_embeddings_dim = final_embeddings_dim
+        self.decoder_dim = decoder_dim
+        self.vocab_size = vocab_size
+        self.dropout = dropout
+        self.choice = choice
+        self.num_layers = num_layers
+        
+        self.embedding = nn.Embedding(vocab_size, self.embed_dim)  # embedding layer
+        self.final_embeddings = nn.Linear(self.embed_dim, final_embeddings_dim)
+        self.dropout = nn.Dropout(p=self.dropout)
+        
+        
+        self.decode_step = nn.LSTMCell(embed_dim, decoder_dim, bias=True)
+        self.init_h = nn.Linear(encoder_dim, decoder_dim)  
+        self.init_c = nn.Linear(encoder_dim, decoder_dim)  
+        
+        self.fc = nn.Linear(decoder_dim, vocab_size)  # linear layer to find scores over vocabulary
+
+        self.init_weights()  # initialize some layers with the uniform distribution
+
+    def init_weights(self):
+        """
+        Initializes some parameters with values from the uniform distribution, for easier convergence.
+        """
+        self.embedding.weight.data.uniform_(-0.1, 0.1)
+        self.fc.bias.data.fill_(0)
+        self.fc.weight.data.uniform_(-0.1, 0.1)
+
+    def load_pretrained_embeddings(self, embeddings):
+        """
+        Loads embedding layer with pre-trained embeddings.
+
+        :param embeddings: pre-trained embeddings
+        """
+        self.embedding.weight = nn.Parameter(embeddings)
+
+
+    def fine_tune_embeddings(self, fine_tune=True):
+        """
+        Allow fine-tuning of embedding layer? (Only makes sense to not-allow if using pre-trained embeddings).
+
+        :param fine_tune: Allow?
+        """
+        for p in self.embedding.parameters():
+            p.requires_grad = fine_tune
+
+    def init_hidden_state(self, encoder_out):
+        """
+        Creates the initial hidden and cell states for the decoder's LSTM based on the encoded images.
+
+        :param encoder_out: encoded images, a tensor of dimension (batch_size, num_pixels, encoder_dim)
+        :return: hidden state, cell state
+        """
+        h = self.init_h(mean_encoder_out)  # (batch_size, decoder_dim)
+        c = self.init_c(mean_encoder_out)
+        return h, c
+        
+    def forward(self, encoder_out, encoded_captions, caption_lengths):
+        """
+        Forward propagation.
+
+        :param encoder_out: encoded images, a tensor of dimension (batch_size, encoder_dim)
+        :param encoded_captions: encoded captions, a tensor of dimension (batch_size, max_caption_length)
+        :param caption_lengths: caption lengths, a tensor of dimension (batch_size, 1)
+        :return: scores for vocabulary, sorted encoded captions, decode lengths, weights, sort indices
+        """
+        batch_size = encoder_out.size(0)
+        encoder_dim = encoder_out.size(-1)
+        vocab_size = self.vocab_size
+
+        # Flatten image
+        encoder_out = encoder_out.view(batch_size, encoder_dim)  
+
+        caption_lengths, sort_ind = caption_lengths.squeeze(1).sort(dim=0, descending=True)
+        encoder_out = encoder_out[sort_ind]
+        encoded_captions = encoded_captions[sort_ind]
+
+        embeddings = self.embedding(encoded_captions) 
+        h, c = self.init_hidden_state(encoder_out)    
+
+        # We won't decode at the <end> position, since we've finished generating as soon as we generate <end>
+        # So, decoding lengths are actual lengths - 1
+        decode_lengths = (caption_lengths - 1).tolist()
+
+        # Create tensors to hold word predicion scores and alphas
+        predictions = torch.zeros(batch_size, max(decode_lengths), vocab_size).to(device)
+
+        for t in range(max(decode_lengths)):
+            batch_size_t = sum([l > t for l in decode_lengths])
+            h, c = self.decode_step(embeddings[:batch_size_t, t, :],(h[:batch_size_t], c[:batch_size_t]))     # (batch_size_t, decoder_dim)
+
+            preds = self.fc(self.dropout(h))  # (batch_size_t, vocab_size)
+
+            predictions[:batch_size_t, t, :] = preds
+            
+        return predictions, encoded_captions, decode_lengths, sort_ind                 
